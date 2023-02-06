62b71e9b1b8fd29af3f55c0e5a4d612421be48c0
Amansinha2023 <mansinha2258@gmail.com>
2023-02-06 14:50:40 +0530

    casul commit

--- JavaDsaAssignment3/Sol5Assig3.java
+++ JavaDsaAssignment3/Sol5Assig3.java
@@ -1,5 +1,37 @@
 //Q5 - Find the total number of bits needed to be flipped to convert x to y.
 
+
+import java.util.*;
 public class Sol5Assig3 {
+ public static void main(String[] args) {
+  int count=0;
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter the value of X:-");
+  int x=sc.nextInt();
+  System.out.println("Enter the value of Y");
+  int y=sc.nextInt();
+  if(x<y){
+   while(x<y){
+   x=x<<1;
+   count++;
+   if(x==y){
+    
+    break;
+   }
+   }
+  }
+  else{
+   while(x>y){
+    x=x>>1;
+    count++;
+    if(x==y){
+     
+     break;
+     
+    }
+   }
+  }
+  System.out.println("The no of bits ot be shifted is "+count);
+ }
  
 }
